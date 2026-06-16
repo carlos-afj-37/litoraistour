c15eae640fc8d2c662e41ca60ff927826b875bac
Carlos Alberto <carlos.afj.37@email.com>
2026-06-16 14:54:55 -0300

    Corrige rotas GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import App from "./App";
 import Login from "./pages/Login";
-import Cadastro from "./pages/Cadastro";
-import Admin from "./pages/Admin";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/litoraistour">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/admin" element={<Admin />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
